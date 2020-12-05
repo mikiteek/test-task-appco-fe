4c10da2879bd9bc1bc06c9656e0cc0883b60bf8d
mikiteek <mikiteek@gmail.com>
2020-12-05 10:37:08 +0200

    Create Logo components for header and footer

---
+++ src/components/Logos/LogoFooterPages/LogoFooterPages.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import styles from "./LogoFooterPages.module.scss";
+
+class LogoFooterPages extends Component {
+  render() {
+    return (
+      <span className={styles.logoFooter}>
+        AppCo
+      </span>
+    );
+  }
+}
+
+export default LogoFooterPages
