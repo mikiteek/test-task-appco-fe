eeeebdd838c2c7d8607ca7c73639d52927ddfeb9
mikiteek <mikiteek@gmail.com>
2020-12-06 11:21:22 +0200

    Styles for table with users statistics

--- src/components/TableUsers/TableUsers.js
+++ src/components/TableUsers/TableUsers.js
@@ -10,45 +10,47 @@ class TableUsers extends Component {
     this.props.onGetUsers(1, 25);
   }
 
-  handleClickByRow(id) {
-    this.props.history.push(`/users/${id}`);
+  handleClickByRow(event) {
+    const {target} = event;
+    const parent = target.parentNode;
+    const refId = parent.querySelector(".js-idCol");
+    this.props.history.push(`/users/${refId.textContent}`);
   }
+
 
   render() {
     const {users: {docs}} = this.props;
     return (
       <div className={styles.tableBlock}>
-        <h2>Users Statistics</h2>
-        {
-          <table>
-            <thead>
-            <tr>
-              <th>Id</th>
-              <th>First name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Gender</th>
-              <th>IP Address</th>
-              <th>Total clicks</th>
-              <th>Total page views</th>
+        <h2 className={styles.tableTitle}>Users Statistics</h2>
+        <table className={styles.table}>
+          <thead className={styles.rowHead}>
+          <tr>
+            <th className={styles.colHeadId}>Id</th>
+            <th>First name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>IP Address</th>
+            <th>Total clicks</th>
+            <th>Total page views</th>
+          </tr>
+          </thead>
+          <tbody className={styles.tableBody}>
+          {docs && docs.map(({id, first_name, last_name, email, gender, ip_address, statistic}) => (
+            <tr key={id} className={styles.userRow} onClick={this.handleClickByRow.bind(this)}>
+              <td className="js-idCol">{id}</td>
+              <td>{first_name}</td>
+              <td>{last_name}</td>
+              <td>{email}</td>
+              <td>{gender}</td>
+              <td>{ip_address}</td>
+              <td>{statistic[0].total_clicks}</td>
+              <td>{statistic[0].total_page_views}</td>
             </tr>
-            </thead>
-            <tbody>
-            {docs && docs.map(({id, first_name, last_name, email, gender, ip_address, statistic}) => (
-              <tr key={id} className={styles.userRow} onClick={() => this.handleClickByRow(id)}>
-                <td>{id}</td>
-                <td>{first_name}</td>
-                <td>{last_name}</td>
-                <td>{email}</td>
-                <td>{gender}</td>
-                <td>{ip_address}</td>
-                <td>{statistic[0].total_clicks}</td>
-                <td>{statistic[0].total_page_views}</td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
-        }
+          ))}
+          </tbody>
+        </table>
       </div>
     );
   }
